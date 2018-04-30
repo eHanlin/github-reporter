85177f165dd494d57205b4f2da48558ec17a6d05
Sparrow.jang <sparrow.jang@gmail.com>
2018-04-30 20:48:25 +0800

    Init project

---
+++ index.js
@@ -0,0 +1,89 @@
+
+var request = require('request');
+var querystring = require('querystring');
+var ACCESS_TOKEN = '';
+var HOST = 'https://api.github.com/'
+var headers = {'User-Agent':'githubReporter'};
+
+function buildHttpPromise(options) {
+    return new Promise(function(resolve, reject) {
+        request(options, function(error, response, body){
+            if (error) reject(body);
+            resolve(JSON.parse(body));
+        });
+    });
+}
+
+function getIssue(org, repo, number) {
+    var url = HOST + 'repos/' + org + '/' + repo +  '/issues/' + number + '?access_token=' + ACCESS_TOKEN;
+    var options = {
+        url:url,
+        headers:headers
+    };
+    return buildHttpPromise(options);
+}
+
+function getIssueNumber(mesage) {
+    var result = /#([0-9]+)/.exec(mesage);
+    return result ? result[1] : null;
+}
+
+function listCommits(own, repo, branch, author, since, until) {
+    var queryString = querystring.stringify({access_token:ACCESS_TOKEN, sha:branch, author:author, since:since, until:until});
+    var url = HOST + 'repos/' + own + '/' + repo +  '/commits?' + queryString;
+    var options = {
+        url:url,
+        headers:headers
+    };
+    return buildHttpPromise(options);
+}
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+function buildCommitPage(own, repo, branch, author, since, until){
+    var queryString = querystring.stringify({author:author, since: since, until: until});
+    return 'https://github.com/' + own + '/' + repo + '/commits/' + branch + '?' + queryString;
+}
+
+function listReport(own, repo, branch, author, since, until) {
+    return listCommits(own, repo, branch, author, since, until)
+
+    .then(function(commits) {
+        var messages = commits.map(function(commit){return commit.commit.message;});
+
+        var promises = messages
+        .map(function(message){
+            return getIssueNumber(message);
+        })
+        .filter(function(number) {
+            return number != null;
+        })
+        .filter(onlyUnique)
+        .map(function(number){
+            return getIssue(own, repo, number)
+        });
+        return Promise.all(promises).then(function(issues){
+            var result = {
+                commitPage:buildCommitPage(own, repo, branch, author, since, until),
+                commitCount:commits.length,
+                commitMessages: commits.map(function(commit){ return commit.commit.message; })
+            };
+            result.issues = issues.filter(function(issue) {
+                return issue.title != null;
+            }).map(function(issue) {
+                return {
+                    title:issue.title
+                }
+            });
+            return result;
+        });
+    });
+}
+
+exports.listReport = listReport;
+exports.setAccessToken = function(accessToken) {
+    ACCESS_TOKEN = accessToken;
+};
+
